33ee09c5b2e5e0f8d90269f41f747f67f75fc1a7
Myszax <myszax14@gmail.com>
2023-04-05 20:21:45 +0200

    Initialize `Conversation` model

--- WPFUI/Models/Conversation.cs
+++ WPFUI/Models/Conversation.cs
@@ -1,6 +1,15 @@
-﻿namespace WPFUI.Models
+﻿using WPFUI.Components;
+
+namespace WPFUI.Models;
+
+public sealed class Conversation
 {
-    class Conversation
-    {
-    }
+    public string Name { get; set; }
+    public string Text { get; set; }
+    public string Sound { get; set; }
+    public string Context { get; set; }
+    public string NpcName { get; set; }
+    public ConversationType Type { get; set; }
+    public int Voice { get; set; }
+    public int Number { get; set; }
 }
