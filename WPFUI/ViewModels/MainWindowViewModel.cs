556fe6ce057cbaf3a8f65458dd7c36cf3a0846d4
Myszax <myszax14@gmail.com>
2023-04-05 23:27:41 +0200

    Add `Parser` to `WPFUI` project reference Temporary parse dialogues on application startup

--- WPFUI/ViewModels/MainWindowViewModel.cs
+++ WPFUI/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Parser;
+using System;
+using System.Collections.Generic;
 
 namespace WPFUI.ViewModels;
 
@@ -6,4 +9,20 @@ public partial class MainWindowViewModel : ObservableObject
 {
     [ObservableProperty]
     private object _selectedTreeItem;
+
+    private List<Dialogue> _parsedDialogues = new();
+
+    public MainWindowViewModel()
+    {
+        var path = @"../../../../Parser/g2nk_ou.bin";
+        var parser = new Reader(path, 1250);
+
+        try
+        {
+            _parsedDialogues = parser.Parse();
+        }
+        catch (Exception)
+        {
+        }
+    }
 }
