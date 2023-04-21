424280b4cd9341f5a2a78fdd83b3a8ae4790fb39
Myszax <myszax14@gmail.com>
2023-04-21 16:27:46 +0200

    Added `Messages` with constructed MessageBoxes

---
+++ WPFUI/Components/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace WPFUI.Components;
+
+public static class Messages
+{
+    public const string CAPTION_AUDIO = "Audio";
+    public const string AUDIO_PATH_NOT_SPECIFIED = "Please specify path to audio files.";
+    public const string FILE_NOT_FOUND = "File not exists.";
+
+    public static void FileNotFound() => MessageBox.Show(FILE_NOT_FOUND, CAPTION_AUDIO, MessageBoxButton.OK, MessageBoxImage.Warning);
+    public static void AudioPathNotSpecified() => MessageBox.Show(AUDIO_PATH_NOT_SPECIFIED, CAPTION_AUDIO, MessageBoxButton.OK, MessageBoxImage.Information);
+}
