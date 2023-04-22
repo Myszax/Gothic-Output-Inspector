7acf533056b7082bda8606209abf62a1a93f07ba
Myszax <myszax14@gmail.com>
2023-04-22 18:12:26 +0200

    Added new model `EncodingMenuItem`

---
+++ WPFUI/Models/EncodingMenuItem.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text;
+
+namespace WPFUI.Models;
+public partial class EncodingMenuItem : ObservableObject
+{
+    public Encoding Encoding { get; set; }
+
+    [ObservableProperty]
+    private bool _isChecked = false;
+}
