7cdfab6e51d2123bbd2697e36591cf840f72cab1
Myszax <myszax14@gmail.com>
2023-04-23 21:26:05 +0200

    Added `RangeObservableCollection` that derives `ObservableCollection` It implements `AddRange()` method that increases speed of adding multiple items by suppressing `OnCollectionChanged` event.

---
+++ WPFUI/Components/RangeObservableCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace WPFUI.Components;
+
+public class RangeObservableCollection<T> : ObservableCollection<T>
+{
+    private bool _suppressNotification = false;
+
+    protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+    {
+        if (!_suppressNotification)
+            base.OnCollectionChanged(e);
+    }
+
+    public void AddRange(IEnumerable<T> list)
+    {
+        if (list is null)
+            throw new ArgumentNullException(nameof(list));
+
+        _suppressNotification = true;
+
+        foreach (T item in list)
+            Add(item);
+
+        _suppressNotification = false;
+        OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+    }
+}
