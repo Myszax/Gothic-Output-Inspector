eb36f35161ac0536d5859538ad0b16f5d8d77db5
Myszax <myszax14@gmail.com>
2023-04-24 23:45:45 +0200

    Added `BindingEnums` class. Now bindings to Enums are easier.

---
+++ WPFUI/Components/BindingEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPFUI.Components;
+
+class BindingEnums : MarkupExtension
+{
+    public Type EnumType { get; }
+
+    public BindingEnums(Type enumType)
+    {
+        if (enumType is null || !enumType.IsEnum)
+            throw new Exception("EnumType cannot be null and of type Enum");
+
+        EnumType = enumType;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider) => Enum.GetValues(EnumType);
+}
