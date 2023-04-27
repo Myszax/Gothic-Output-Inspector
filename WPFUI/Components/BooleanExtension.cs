3fa31f738de8b53b291f7604b6436f2311b02da5
Myszax <myszax14@gmail.com>
2023-04-27 18:28:09 +0200

    New Boolean `MarkupExtension`. It's simplifying using bools in XAML

---
+++ WPFUI/Components/BooleanExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace WPFUI.Components;
+
+public class TrueExtension : MarkupExtension
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => true;
+}
+
+public class FalseExtension : MarkupExtension
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => false;
+}
