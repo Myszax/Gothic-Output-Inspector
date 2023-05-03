a5068e212ae76930e7ac2c7090ebca11cc8c68a5
Myszax <myszax14@gmail.com>
2023-05-03 19:37:28 +0200

    New `SaveFile` class with its source gen equivalent

---
+++ WPFUI/Components/SaveFile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WPFUI.Models;
+
+namespace WPFUI.Components;
+
+public sealed class SaveFile
+{
+    public int Version { get; set; } = 1;
+
+    public List<Conversation> Conversations { get; set; } = new();
+
+    public string OriginalEncoding { get; set; } = string.Empty;
+
+    public string ChosenEncoding { get; set; } = string.Empty;
+
+    public string AudioPath { get; set; } = string.Empty;
+}
---
+++ WPFUI/Components/SaveFileJsonContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WPFUI.Components;
+
+[JsonSerializable(typeof(SaveFile))]
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Default)]
+public partial class SaveFileJsonContext : JsonSerializerContext
+{
+}
