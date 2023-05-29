c2a78da442be63d4d19c72162a01ea12a9b97c83
Myszax <myszax14@gmail.com>
2023-05-30 00:31:45 +0200

    Added MuteButtonConverter with icons

---
+++ WPFUI/Converters/MuteButtonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFUI.Converters
+{
+    public sealed class MuteButtonConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Boolean muted)
+                return value;
+
+            if (muted)
+                return "Solid_VolumeXmark";
+
+            return "Solid_VolumeHigh";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
