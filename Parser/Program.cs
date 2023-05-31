5e05d541d24a983f427dea7262dfdcbe0867c616
Myszax <myszax14@gmail.com>
2023-05-31 12:28:42 +0200

    Change Parser project to Library type

--- Parser/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Parser;
-using System.Text;
-
-var path = @"../../../g2nk_ou.bin";
-
-var reader = new Reader(path, Encoding.GetEncoding(1250));
-var list = reader.Parse();
-
-Console.ReadLine();
