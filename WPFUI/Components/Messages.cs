45e51e6b5dc01d5e43f65bdbf5f1af7c7a4a9466
Myszax <myszax14@gmail.com>
2023-05-31 16:33:28 +0200

    Change assembly name and title

--- WPFUI/Components/Messages.cs
+++ WPFUI/Components/Messages.cs
@@ -7,7 +7,7 @@ public static class Messages
     public const string CAPTION_AUDIO = "Audio";
     public const string AUDIO_PATH_NOT_SPECIFIED = "Please specify path to audio files.";
     public const string FILE_NOT_FOUND = "File not exists.";
-    public const string TITLE = "Gothic OU Inspector";
+    public const string TITLE = "Gothic Output Inspector";
     public const string CAPTION_SAVING = "Saving";
     public const string SAVE_PROMPT = "Do you want to save project?";
 
