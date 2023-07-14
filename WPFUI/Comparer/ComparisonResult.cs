687c3fcc1203ef6e9136adf9e8779ba035c857ca
Myszax <myszax14@gmail.com>
2023-07-14 13:29:48 +0200

    Added `ComparisonResult` class that stores information about object differences

---
+++ WPFUI/Comparer/ComparisonResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using WPFUI.Enums;
+
+namespace WPFUI.Comparer;
+
+public sealed class ComparisonResult<T>
+{
+    public T? Original { get; set; }
+    public T? Compared { get; set; }
+    public ComparisonResultType Type { get; set; }
+    public IDictionary<string, ComparisonVariance> Variances { get; set; }
+
+    public ComparisonResult(T? original, T? compared, ComparisonResultType type, IDictionary<string, ComparisonVariance> variances)
+    {
+        Original = original;
+        Compared = compared;
+        Type = type;
+        Variances = variances;
+    }
+}
---
+++ WPFUI/Enums/ComparisonResultType.cs
@@ -0,0 +1,8 @@
+﻿namespace WPFUI.Enums;
+
+public enum ComparisonResultType
+{
+    Added,
+    Removed,
+    Changed
+}
