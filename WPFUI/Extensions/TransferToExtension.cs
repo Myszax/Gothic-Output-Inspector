1cfa98d3151bf9b865de1bbcff91fe845ce10b47
Myszax <myszax14@gmail.com>
2023-07-16 13:56:44 +0200

    New `TransferTo` method that copy values from souce object to target object via reflection

---
+++ WPFUI/Extensions/TransferToExtension.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Reflection;
+using WPFUI.Comparer;
+
+namespace WPFUI.Extensions;
+
+public static class TransferToExtension
+{
+    public static void TransferTo<T>(this T source, T target, IDictionary<string, ComparisonVariance> values)
+    {
+        if (values is null)
+            return;
+
+        var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        if (properties.Length == 0)
+            return;
+
+        var propertiesDict = properties.ToImmutableDictionary(k => k.Name, v => v);
+
+        foreach (var value in values)
+        {
+            if (!propertiesDict.TryGetValue(value.Key, out var property))
+                continue;
+
+            property.SetValue(target, property.GetValue(source));
+        }
+    }
+}
