d8a562753191c5c228cdfeca27f670680d3000c2
Myszax <myszax14@gmail.com>
2024-05-13 22:58:00 +0200

    Feat: Added thousand separator in footer for Edited and Inspected (#10)
    * Added thousand separator for `PercentageCoverageConverter`
    
    * Added space between number and bracket in zero case
    Changed `string` to `var`

--- WPFUI/Converters/PercentageCoverageConverter.cs
+++ WPFUI/Converters/PercentageCoverageConverter.cs
@@ -8,7 +8,7 @@ public sealed class PercentageCoverageConverter : IMultiValueConverter
 {
     public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
     {
-        string zero = $"0({0:N2})%";
+        var zero = $"0 ({0:N2})%";
 
         if (values.Length != 2 || values[0] is not int || values[1] is not int)
             return zero;
@@ -20,7 +20,7 @@ public sealed class PercentageCoverageConverter : IMultiValueConverter
         var currentAmount = (int)values[0];
         var percentageValue = decimal.Divide(currentAmount, totalAmount) * 100;
 
-        return $"{currentAmount}({percentageValue:N2}%)";
+        return string.Create(CultureInfo.InvariantCulture, $"{currentAmount:N0} ({percentageValue:N2}%)");
     }
 
     public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
