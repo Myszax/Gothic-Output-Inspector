b966f1f82c0e1fe5e1bd765efbe9795fccd74451
Myszax <myszax14@gmail.com>
2024-06-26 16:12:03 +0200

    Set `Encoding.Default` in `EncodingMenuItem`

--- WPFUI/Models/EncodingMenuItem.cs
+++ WPFUI/Models/EncodingMenuItem.cs
@@ -4,7 +4,7 @@ using System.Text;
 namespace WPFUI.Models;
 public partial class EncodingMenuItem : ObservableObject
 {
-    public Encoding Encoding { get; set; }
+    public Encoding Encoding { get; set; } = Encoding.Default;
 
     [ObservableProperty]
     private bool _isChecked = false;
