c9c9703ba750a79284d3061eb810bb762577483f
Myszax <myszax14@gmail.com>
2024-06-26 13:38:59 +0200

    Removed unused properties from `IDataService`

--- WPFUI/Services/IDataService.cs
+++ WPFUI/Services/IDataService.cs
@@ -8,24 +8,14 @@ public interface IDataService
 {
     public RangeObservableCollection<Conversation> Data { get; set; }
     public HashSet<Conversation> ConversationsToCompare { get; set; }
-    public string AudioFilesPath { get; set; }
     public Conversation CurrentConversation { get; set; }
     public string CompareWindowTitle { get; set; }
-
-    public FilterType SelectedFilterTypeCompareMode { get; set; }
-    public bool IsEnabledFilterCompareModeIsInspected { get; set; }
-    public bool IsEnabledIgnoreInspectedWhileTransfer { get; set; }
 }
 
 public sealed class DataService : IDataService
 {
     public RangeObservableCollection<Conversation> Data { get; set; } = [];
     public HashSet<Conversation> ConversationsToCompare { get; set; } = [];
-    public string AudioFilesPath { get; set; } = string.Empty;
     public Conversation CurrentConversation { get; set; } = new();
     public string CompareWindowTitle { get; set; } = string.Empty;
-
-    public FilterType SelectedFilterTypeCompareMode { get; set; } = FilterType.HideAll;
-    public bool IsEnabledFilterCompareModeIsInspected { get; set; } = true;
-    public bool IsEnabledIgnoreInspectedWhileTransfer { get; set; } = true;
 }
