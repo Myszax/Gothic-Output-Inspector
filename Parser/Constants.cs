d1bb2baabbdabe86e0d9ad7b3ce564a6688887bd
Myszax <myszax14@gmail.com>
2024-06-27 22:46:08 +0200

    Reorder region in `Constants`

--- Parser/Constants.cs
+++ Parser/Constants.cs
@@ -2,6 +2,14 @@
 
 public static class Constants
 {
+    #region Parser_Numbers
+    public const int MAXIMUM_BLOCK_COUNT_READED = 1;
+    public const int MINIMUM_OBJECT_LENGTH = 2;
+    public const long MINIMUM_REMAINING_BYTES_TO_READ_OBJECT = 6;
+    public const ushort OBJECT_END_LENGTH = 2;
+    public const int PARSED_OBJECT_ELEMENTS_COUNT = 4;
+    #endregion
+
     #region ZenGine_Archive_Formats
     public const string ARCHIVE_FORMAT_ASCII = "ASCII";
     public const string ARCHIVE_FORMAT_BIN_SAFE = "BIN_SAFE";
@@ -23,13 +31,5 @@ public static class Constants
     public const string zCCSLib = "zCCSLib";
     public const string zCEventMessage = "oCMsgConversation:oCNpcMessage:zCEventMessage";
     public const string zOBJECT_END = "[]";
-    #endregion
-
-    #region Parser_Numbers
-    public const int MAXIMUM_BLOCK_COUNT_READED = 1;
-    public const int MINIMUM_OBJECT_LENGTH = 2;
-    public const long MINIMUM_REMAINING_BYTES_TO_READ_OBJECT = 6;
-    public const ushort OBJECT_END_LENGTH = 2;
-    public const int PARSED_OBJECT_ELEMENTS_COUNT = 4;
     #endregion
 }
