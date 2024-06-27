c04912e5997ec9737d655df5e2a290c0ad6d41b0
Myszax <myszax14@gmail.com>
2024-06-27 22:48:53 +0200

    Quick reoder for `ArchiveObject`

--- Parser/FileBlocks/ArchiveObject.cs
+++ Parser/FileBlocks/ArchiveObject.cs
@@ -2,8 +2,8 @@
 
 internal sealed class ArchiveObject
 {
-    public string ObjectName { get; set; } = string.Empty;
     public string ClassName { get; set; } = string.Empty;
-    public short Version { get; set; }
     public int Index { get; set; }
+    public string ObjectName { get; set; } = string.Empty;
+    public short Version { get; set; }
 }
