06e5bf245676fb6ecf6db002c0c5f9d41a350d35
Myszax <myszax14@gmail.com>
2024-06-27 21:16:36 +0200

    Quick reoder for `WaveStreamExtensions`

--- WPFUI/NAudioWrapper/WaveStreamExtensions.cs
+++ WPFUI/NAudioWrapper/WaveStreamExtensions.cs
@@ -18,9 +18,7 @@ public static class WaveStreamExtensions
         stream.Position = newPos;
     }
 
-    public static void SetPosition(this WaveStream stream, double seconds) => stream.SetPosition((long)(seconds * stream.WaveFormat.AverageBytesPerSecond));
-
-    public static void SetPosition(this WaveStream stream, TimeSpan time) => stream.SetPosition(time.TotalSeconds);
-
     public static void Seek(this WaveStream stream, double offset) => stream.SetPosition(stream.Position + (long)(offset * stream.WaveFormat.AverageBytesPerSecond));
+    public static void SetPosition(this WaveStream stream, double seconds) => stream.SetPosition((long)(seconds * stream.WaveFormat.AverageBytesPerSecond));
+    public static void SetPosition(this WaveStream stream, TimeSpan time) => stream.SetPosition(time.TotalSeconds);
 }
