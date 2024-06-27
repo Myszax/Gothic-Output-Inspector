58c4a0a2ef338d6957337fcd582b8390b1dcdb34
Myszax <myszax14@gmail.com>
2024-06-27 22:25:45 +0200

    Quick reoder for `ComparisonResult`

--- WPFUI/Comparer/ComparisonResult.cs
+++ WPFUI/Comparer/ComparisonResult.cs
@@ -5,8 +5,8 @@ namespace WPFUI.Comparer;
 
 public sealed class ComparisonResult<T>(T? original, T? compared, ComparisonResultType type, IDictionary<string, ComparisonVariance> variances)
 {
-    public T? Original { get; set; } = original;
     public T? Compared { get; set; } = compared;
+    public T? Original { get; set; } = original;
     public ComparisonResultType Type { get; set; } = type;
     public IDictionary<string, ComparisonVariance> Variances { get; set; } = variances;
 }
