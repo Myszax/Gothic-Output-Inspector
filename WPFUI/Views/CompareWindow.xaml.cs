9f1affbb9c5dd49e45fe92b960998a716f881127
Myszax <myszax14@gmail.com>
2024-06-30 23:04:43 +0200

    Feat: Rearranged DetailedPanel in normal and compare mode, added Copy to Clipboard buttons (#21)
    * Changed properties of `SmoothTexBox`
    
    * Added new style `CopyButton`
    
    * Added `CopyToClipboard` command
    
    * Reworked `DetailedPanel`
    Added `Copy to Clipboard` buttons
    
    * Added tooltip to `CopyButton`
    
    * Removed unused method
    
    * Rearranged panel with properties in `CompareWindow`
    Added `CopyButton`s to `CompareWindow`
    
    * Added info what type of difference is selected
    
    * Removed usued method
    
    * Renamed TexBoxe names to avoid conflicts

--- WPFUI/Views/CompareWindow.xaml.cs
+++ WPFUI/Views/CompareWindow.xaml.cs
@@ -8,12 +8,4 @@ public partial class CompareWindow : Window
     {
         InitializeComponent();
     }
-
-    private void TextBox_CopyToClipboard(object sender, System.Windows.Input.MouseButtonEventArgs e)
-    {
-        if (sender is not System.Windows.Controls.TextBox textBox || string.IsNullOrEmpty(textBox.Text))
-            return;
-
-        Clipboard.SetText(textBox.Text);
-    }
 }
